cc50aeaafd5485d0f0aaa8b31ddc7b9d6fa23dd3
Sergey Vynogradov <sergey.vynogradov@gen2fund.com>
2024-06-06 02:35:49 +0300

    Async calls from Ruby client

--- client.rb
+++ client.rb
@@ -1,36 +1,16 @@
 require 'openssl'
 require 'faraday'
+require 'async'
+require 'async/semaphore'
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-# Есть три типа эндпоинтов API
-# Тип A:
-#   - работает 1 секунду
-#   - одновременно можно запускать не более трёх
-# Тип B:
-#   - работает 2 секунды
-#   - одновременно можно запускать не более двух
-# Тип C:
-#   - работает 1 секунду
-#   - одновременно можно запускать не более одного
-#
-def a(value)
-  puts "https://localhost:9292/a?value=#{value}"
-  Faraday.get("https://localhost:9292/a?value=#{value}").body
+def async_request(endpoint, value, semaphore)
+  semaphore.async do
+    puts "https://localhost:9292/#{endpoint}?value=#{value}"
+    Faraday.get("https://localhost:9292/#{endpoint}?value=#{value}").body
+  end
 end
-
-def b(value)
-  puts "https://localhost:9292/b?value=#{value}"
-  Faraday.get("https://localhost:9292/b?value=#{value}").body
-end
-
-def c(value)
-  puts "https://localhost:9292/c?value=#{value}"
-  Faraday.get("https://localhost:9292/c?value=#{value}").body
-end
-
-# Референсное решение, приведённое ниже работает правильно, занимает ~19.5 секунд
-# Надо сделать в пределах 7 секунд
 
 def collect_sorted(arr)
   arr.sort.join('-')
@@ -38,41 +18,50 @@ end
 
 start = Time.now
 
-a11 = a(11)
-a12 = a(12)
-a13 = a(13)
-b1 = b(1)
+Async do |task|
+  sem_a = Async::Semaphore.new(3)
+  sem_b = Async::Semaphore.new(2)
+  sem_c = Async::Semaphore.new(1)
 
-ab1 = "#{collect_sorted([a11, a12, a13])}-#{b1}"
-puts "AB1 = #{ab1}"
+  # First batch of 'a' requests
+  a11 = async_request('a', 11, sem_a)
+  a12 = async_request('a', 12, sem_a)
+  a13 = async_request('a', 13, sem_a)
 
-c1 = c(ab1)
-puts "C1 = #{c1}"
+  # Second batch of 'a' requests
+  a21 = async_request('a', 21, sem_a)
+  a22 = async_request('a', 22, sem_a)
+  a23 = async_request('a', 23, sem_a)
 
-a21 = a(21)
-a22 = a(22)
-a23 = a(23)
-b2 = b(2)
+  # Third batch of 'a' requests
+  a31 = async_request('a', 31, sem_a)
+  a32 = async_request('a', 32, sem_a)
+  a33 = async_request('a', 33, sem_a)
 
-ab2 = "#{collect_sorted([a21, a22, a23])}-#{b2}"
-puts "AB2 = #{ab2}"
+  # Handle 'b' requests separately to maximize parallelism
+  b1 = async_request('b', 1, sem_b)
+  b2 = async_request('b', 2, sem_b)
+  b3 = async_request('b', 3, sem_b)
 
-c2 = c(ab2)
-puts "C2 = #{c2}"
+  # Wait for all 'a' requests to complete and process 'ab' combinations
+  ab1 = task.async { "#{collect_sorted([a11.wait, a12.wait, a13.wait])}-#{b1.wait}" }
+  ab2 = task.async { "#{collect_sorted([a21.wait, a22.wait, a23.wait])}-#{b2.wait}" }
+  ab3 = task.async { "#{collect_sorted([a31.wait, a32.wait, a33.wait])}-#{b3.wait}" }
 
-a31 = a(31)
-a32 = a(32)
-a33 = a(33)
-b3 = b(3)
+  # Handle 'c' requests as soon as the corresponding 'ab' is ready
+  c1 = task.async { async_request('c', ab1.wait, sem_c).wait }
+  c2 = task.async { async_request('c', ab2.wait, sem_c).wait }
+  c3 = task.async { async_request('c', ab3.wait, sem_c).wait }
 
-ab3 = "#{collect_sorted([a31, a32, a33])}-#{b3}"
-puts "AB3 = #{ab3}"
+  # Collect results of all 'c' requests
+  c1 = c1.wait
+  c2 = c2.wait
+  c3 = c3.wait
 
-c3 = c(ab3)
-puts "C3 = #{c3}"
+  # Final 'a' request with sorted 'c' results
+  c123 = collect_sorted([c1, c2, c3])
+  result = async_request('a', c123, sem_a).wait
 
-c123 = collect_sorted([c1, c2, c3])
-result = a(c123)
-
-puts "FINISHED in #{Time.now - start}s."
-puts "RESULT = #{result}" # 0bbe9ecf251ef4131dd43e1600742cfb
+  puts "FINISHED in #{Time.now - start}s."
+  puts "RESULT = #{result}"
+end
